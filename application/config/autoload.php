9bcaf6c1b4e149ce03f734ed9a7e813336c1b0de
shahajahancse <msacse1@gmail.com>
2022-12-15 11:08:00 +0600

    some change

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'form','language','text','debug_helper', 'BanglaDate');
+$autoload['helper'] = array('url', 'file', 'form','language','text','debug_helper', 'bangladate');
 
 /*
 | -------------------------------------------------------------------
